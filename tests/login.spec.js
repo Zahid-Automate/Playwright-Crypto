52558d35e8ebf180231961097d18e5d1d8462071
mzahid <abdulzahid.01@gmail.com>
2024-11-06 20:29:42 -0800

    Updated test script to read contents from .env file

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,26 +1,27 @@
 const { test, expect } = require('@playwright/test');
+const encryption = require('../utils/encryption');
 require('dotenv').config();
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  console.log(process.env.UN);
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 });
-
 
 test.describe('Login Validation', () => {
     test('Able to login successfully', async ({ page }) => {
-      // create a new todo locator
-      await page.getByPlaceholder('Username').click();
-      await page.getByPlaceholder('Username').fill(process.env.UN);
-      await page.getByPlaceholder('Password').click();
-      await page.getByPlaceholder('Password').fill(process.env.PASSWORD);
-      await page.getByRole('button', { name: 'Login' }).click();
-      await page.pause();
-    });
-  });
-  
-  test.afterEach(async ({ page }) => {
-  await page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
-  await page.getByRole('menuitem', { name: 'Logout' }).click();
+        const username = encryption.decrypt(process.env.ENCRYPTED_UN);
+        const password = encryption.decrypt(process.env.ENCRYPTED_PASSWORD);
 
+        // Use the decrypted credentials
+        await page.getByPlaceholder('Username').click();
+        await page.getByPlaceholder('Username').fill(username);
+        await page.getByPlaceholder('Password').click();
+        await page.getByPlaceholder('Password').fill(password);
+        await page.getByRole('button', { name: 'Login' }).click();
+        await page.pause();
+    });
+});
+
+test.afterEach(async ({ page }) => {
+    await page.getByRole('banner').getByRole('img', { name: 'profile picture' }).click();
+    await page.getByRole('menuitem', { name: 'Logout' }).click();
 });
